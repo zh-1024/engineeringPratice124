45abff7bde00a6a192de502fcda9854069ad1418
zz <1923991238@qq.com>
2022-01-28 22:49:40 +0800

    generator pojo

--- src/main/java/com/fourzhang/youddit/mapper/UserMapper.java
+++ src/main/java/com/fourzhang/youddit/mapper/UserMapper.java
@@ -2,8 +2,9 @@ package com.fourzhang.youddit.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fourzhang.youddit.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
-
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
     
 }
