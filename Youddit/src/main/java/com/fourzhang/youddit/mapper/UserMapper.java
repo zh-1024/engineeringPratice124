5dc81ff3739c2ed7bb705b4c4745d013b837ff85
zh-1024 <64407148+zh-1024@users.noreply.github.com>
2022-01-28 16:10:09 +0800

    Delete Youddit directory

--- Youddit/src/main/java/com/fourzhang/youddit/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.fourzhang.youddit.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.fourzhang.youddit.entity.User;
-
-
-public interface UserMapper extends BaseMapper<User> {
-    
-}
