4063f567ad09ba9dd17d96840d3cc3ebc94ae313
zz <1923991238@qq.com>
2022-03-06 20:19:17 +0800

    查询发布帖子和收藏内容

---
+++ src/main/java/com/fourzhang/youddit/service/ContentService.java
@@ -0,0 +1,15 @@
+package com.fourzhang.youddit.service;
+
+import com.fourzhang.youddit.data.Result;
+import com.fourzhang.youddit.response.ContentResponse;
+
+import java.security.Principal;
+
+/**
+ * @author zh
+ * TODO: 此模块包含查询个人帖子、收藏帖子和他人帖子相关接口
+ */
+public interface ContentService {
+    public Result<ContentResponse> getSelfPublishContents(Long currentPage, Long pageSize, Principal principal);
+    public Result<ContentResponse> getSelfCollectContents(Long currentPage,Long pageSize,Principal principal);
+}
