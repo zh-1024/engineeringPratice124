c43145d2d62c20cb94bd37a1e6dfd2d9c9df11a2
leniy2013 <2846782902@qq.com>
2022-06-21 15:48:41 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/fourzhang/youddit/config/WebMVCConfig.java
+++ src/main/java/com/fourzhang/youddit/config/WebMVCConfig.java
@@ -1,5 +1,5 @@
-package com.zh.music.config;
-
+//package com.zh.music.config;
+package com.fourzhang.youddit.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
