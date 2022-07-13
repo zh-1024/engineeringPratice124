b1a78fe2e3fe30e43b7ab854c88c10e1e4a65805
Youtirsin <1711221318@qq.com>
2022-07-13 21:11:44 +0800

    final update

--- README.md
+++ README.md
@@ -1,10 +1,146 @@
-# 一、概述
-    工程实践
-# 二、测试
-```java
-public class Demo{
-    public void test(){
-        System.out.println("");
-    }
-}
+# Youddit 图片兴趣社区服务端
+
+服务端项目主要使用Java语言和SpringBoot框架。可以采取测试运行和使用发行版本两种方式来分别测试和部署服务端程序。由于生产环境的不同，建议手动配置和构建**适用于目标生产环境的发行版本**。
+
+## 预备条件
+
+项目服务端与客户端依赖以下程序和中间件。在进一步操作前请确保所有依赖项的正确安装。
+
+1. JDK17
+   
+   - 用于测试运行和构建服务端程序
+   
+   - 生产环境建议使用JRE17即可
+   
+   - [Microsoft Build of OpenJDK](https://www.microsoft.com/openjdk)
+
+2. Maven
+   
+   - 依赖管理和构建工具
+   - [Maven – Welcome to Apache Maven](https://maven.apache.org/)
+
+3. MySQL
+   
+   - 数据库
+   - [MySQL :: MySQL Downloads](https://www.mysql.com/downloads/)
+
+4. ErLang
+   
+   - RabbitMQ的依赖
+   
+   - **Windows下请以管理员身份进行安装，并先于RabbitMQ安装**
+   
+   - [Downloads - Erlang/OTP](https://www.erlang.org/downloads)
+
+5. RabbitMQ
+   
+   - 处理私信等的消息中间件
+   - **Windows下请以管理员身份进行安装**
+   - [Messaging that just works — RabbitMQ
+
+## 依赖配置
+
+### MySQL
+
+使用如下命令来创建数据库和数据表。其中mysql路径，用户名，密码，create.sql文件所在路径视具体情况而定。
+
+```bash
+path\to\your\mysql –uroot –p123456 <C:\create.sql
 ```
+
+### RabbitMQ
+
+1. 在RabbitMQ安装目录的的bin目录下执行以下命令打开STOMP插件
+   
+   ```bash
+   rabbitmq-plugins enable rabbitmq_stomp
+   ```
+
+2. 创建虚拟主机youddit
+   
+   ```bash
+   rabbitmqctl add_vhost youddit
+   ```
+
+### 项目配置
+
+修改服务端项目下的/src/resources/application.yml文件，其中附**带注释部分**更改为生产环境具体配置。
+
+```yml
+spring:
+  datasource
+    # database username
+    # 数据库用户名
+    username: root
+    # database password
+    # 数据库密码
+    password: 123456
+    # change localhost to the address of the database
+    # 将localhost更改为数据库服务地址
+    url: jdbc:mysql://${MYSQL_HOST:localhost}:3306/youddit
+    driver-class-name: com.mysql.cj.jdbc.Driver
+  rabbitmq:
+    # RabbitMQ address
+    # RabbitMQ服务地址
+    host: 127.0.0.1
+    port: 5672
+    virtual-host: youddit
+    # RabbitMQ username
+    # RabbitMQ用户名
+    username: guest
+    # RabbitMQ password
+    # RabbitMQ密码
+    password: guest
+  servlet:
+    multipart:
+      max-file-size: 2MB
+      max-request-size: 20MB
+
+
+
+mybatis-plus:
+  mapper-locations: classpath:mapper/*.xml
+management:
+  endpoints:
+    web:
+      exposure:
+        include: refresh
+```
+
+### 测试运行
+
+*测试运行不建议用于生产环境。*
+
+在项目pom.xml目录的终端下运行以下命令测试运行
+
+*运行前请确保已经正确安装JDK17, maven。*
+
+```bash
+mvn clean package
+```
+
+### 生产环境
+
+#### 从源码构建
+
+在pom.xml目录终端下使用以下命令手动构建Jar文件
+
+*流程前请确保机器已正确安装JDK17, maven。*
+
+```bash
+mvn clean package
+```
+
+命令正确执行后可在target目录下找到所需Jar文件
+
+#### 使用Jar文件
+
+*使用发行版本前请确保目标机器已正确安装JRE17(JDK17)。*
+
+1. 在Jar文件所在目录打开终端
+
+2. 执行如下命令
+   
+   ```bash
+   java -jar youddit-0.0.1-SNAPSHOT.jar
+   ```
--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,14 @@
--- table user
+-- ----------------------------
+-- database `youddit`
+-- ----------------------------
+CREATE DATABASE `youddit` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -12,155 +20,38 @@ CREATE TABLE `user` (
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `comment_comment`;
-CREATE TABLE `comment_comment` (
-  `id` bigint NOT NULL,
-  `content_comment_id` bigint NOT NULL,
-  `comment_comment_id` bigint DEFAULT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `comment_time` datetime NOT NULL,
-  `level` tinyint NOT NULL,
+-- ----------------------------
+-- Table structure for `private_message`
+-- ----------------------------
+DROP TABLE IF EXISTS `private_message`;
+CREATE TABLE `private_message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `sender_id` bigint NOT NULL,
+  `receive_id` bigint NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `send_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `label`
+-- ----------------------------
+DROP TABLE IF EXISTS `label`;
+CREATE TABLE `label` (
+  `label_id` bigint NOT NULL AUTO_INCREMENT,
+  `label_name` varchar(50) NOT NULL,
+  `use_num` bigint NOT NULL,
+  PRIMARY KEY (`label_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of comment_comment
--- ----------------------------
-
--- ----------------------------
--- Table structure for `content`
--- ----------------------------
-DROP TABLE IF EXISTS `content`;
-CREATE TABLE `content` (
-  `content_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `info_describe` varchar(255) NOT NULL,
-  `views_num` bigint NOT NULL,
-  `like_num` bigint NOT NULL,
-  `collect_num` bigint NOT NULL,
-  `post_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`content_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of content
--- ----------------------------
-
--- ----------------------------
--- Table structure for `content_collect_user`
--- ----------------------------
-DROP TABLE IF EXISTS `content_collect_user`;
-CREATE TABLE `content_collect_user` (
-  `id` bigint NOT NULL,
-  `content_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of content_collect_user
--- ----------------------------
-
--- ----------------------------
--- Table structure for `content_image`
--- ----------------------------
-DROP TABLE IF EXISTS `content_image`;
-CREATE TABLE `content_image` (
-  `id` bigint NOT NULL,
-  `content_id` bigint NOT NULL,
-  `image_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of content_image
--- ----------------------------
-
--- ----------------------------
--- Table structure for `content_label`
--- ----------------------------
-DROP TABLE IF EXISTS `content_label`;
-CREATE TABLE `content_label` (
-  `id` bigint NOT NULL,
-  `content_id` bigint NOT NULL,
-  `label_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of content_label
--- ----------------------------
-
--- ----------------------------
--- Table structure for `content_like_user`
--- ----------------------------
-DROP TABLE IF EXISTS `content_like_user`;
-CREATE TABLE `content_like_user` (
-  `id` bigint NOT NULL,
-  `content_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of content_like_user
--- ----------------------------
-
--- ----------------------------
--- Table structure for `cotent_comment`
--- ----------------------------
-DROP TABLE IF EXISTS `content_comment`;
-CREATE TABLE `content_comment` (
-  `content_comment_id` bigint NOT NULL,
-  `content_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `comment_info` varchar(255) NOT NULL,
-  `comment_time` datetime NOT NULL,
-  PRIMARY KEY (`content_comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of cotent_comment
--- ----------------------------
-
--- ----------------------------
--- Table structure for `follow`
--- ----------------------------
-DROP TABLE IF EXISTS `follow`;
-CREATE TABLE `follow` (
-  `id` bigint NOT NULL,
-  `user1_id` bigint NOT NULL,
-  `user2_id` bigint NOT NULL,
-  `follow_time` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of follow
--- ----------------------------
-
--- ----------------------------
--- Table structure for `image`
--- ----------------------------
-DROP TABLE IF EXISTS `image`;
-CREATE TABLE `image` (
-  `image_id` bigint NOT NULL,
-  `image_url` varchar(255) NOT NULL,
-  PRIMARY KEY (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of image
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for `inform`
 -- ----------------------------
 DROP TABLE IF EXISTS `inform`;
 CREATE TABLE `inform` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `new_follow_num` bigint DEFAULT NULL,
   `new_private_message_num` bigint DEFAULT NULL,
   `new_comment_num` bigint DEFAULT NULL,
@@ -168,38 +59,111 @@ CREATE TABLE `inform` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Records of inform
+-- Table structure for `image`
 -- ----------------------------
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+  `image_id` bigint NOT NULL AUTO_INCREMENT,
+  `image_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Table structure for `label`
+-- Table structure for `follow`
 -- ----------------------------
-DROP TABLE IF EXISTS `label`;
-CREATE TABLE `label` (
-  `label_id` bigint NOT NULL,
-  `label_name` varchar(50) NOT NULL,
-  `use_num` bigint NOT NULL,
-  PRIMARY KEY (`label_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of label
--- ----------------------------
-
--- ----------------------------
--- Table structure for `private_message`
--- ----------------------------
-DROP TABLE IF EXISTS `private_message`;
-CREATE TABLE `private_message` (
-  `id` bigint NOT NULL,
-  `sender_id` bigint NOT NULL,
-  `receive_id` bigint NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `send_time` datetime NOT NULL,
+DROP TABLE IF EXISTS `follow`;
+CREATE TABLE `follow` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user1_id` bigint NOT NULL,
+  `user2_id` bigint NOT NULL,
+  `follow_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Records of private_message
+-- Table structure for `content_like_user`
 -- ----------------------------
+DROP TABLE IF EXISTS `content_like_user`;
+CREATE TABLE `content_like_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `content_label`
+-- ----------------------------
+DROP TABLE IF EXISTS `content_label`;
+CREATE TABLE `content_label` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `label_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `content_image`
+-- ----------------------------
+DROP TABLE IF EXISTS `content_image`;
+CREATE TABLE `content_image` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `image_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `content_comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `content_comment`;
+CREATE TABLE `content_comment` (
+  `content_comment_id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `comment_info` varchar(255) NOT NULL,
+  `comment_time` datetime NOT NULL,
+  PRIMARY KEY (`content_comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `content_collect_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `content_collect_user`;
+CREATE TABLE `content_collect_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `content`
+-- ----------------------------
+DROP TABLE IF EXISTS `content`;
+CREATE TABLE `content` (
+  `content_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `info_describe` varchar(255) NOT NULL,
+  `views_num` bigint NOT NULL,
+  `like_num` bigint NOT NULL,
+  `collect_num` bigint NOT NULL,
+  `post_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`content_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for `comment_comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment_comment`;
+CREATE TABLE `comment_comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content_comment_id` bigint NOT NULL,
+  `comment_comment_id` bigint DEFAULT NULL,
+  `user_id` bigint NOT NULL,
+  `comment_time` datetime NOT NULL,
+  `level` tinyint NOT NULL,
+  `comment_info` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
